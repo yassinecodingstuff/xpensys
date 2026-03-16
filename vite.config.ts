a570a95476a7914295e2e97ff0fc558f06323595
EL GARTI YASSINE <yass.elgarti@gmail.com>
2026-03-16 23:58:55 +0100

    fix2

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: AudioProcessingEvent.env.VITE_BASE_PATH || "/xpensys",
+  base: process.env.VITE_BASE_PATH || "/xpensys",
   server: {
     proxy: {
       "/api": {
